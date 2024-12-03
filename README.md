6ded50f7901b36476f7710aebec1a477038ef668
Anubhav Singh <anubhavsinghbkj@gmail.com>
2024-12-03 14:49:14 +0530

    Complete Typescript

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # TypeScript
-#   1 Types of languages
-#   2 What is Typescript
-#   3 The tsc compiler
-#   4 Basic Types in TypeScript  
-#   5 The tsconfig file
-#   6 Interfaces=>important
-#   7 Type=>important
-#   8 Array in TS
-#
+#   1 Types of languages✅
+#   2 What is Typescript✅
+#   3 The tsc compiler✅
+#   4 Basic Types in TypeScript✅  
+#   5 The tsconfig file✅
+#   6 Interfaces=>important✅
+#   7 Type=>important✅
+#   8 Array in TS✅
+#   9 Enums✅
+#   10 Generics✅
+#   11 Exporting and importing modules✅
+
+<!-- Resources Link -->
+[Link](https://projects.100xdevs.com/tracks/6SbPPXGkG8QKFOTW9BmL/ts-1)
--- src/a.ts
+++ src/a.ts
@@ -1,107 +1,160 @@
-function greet(firstName: string){
-    console.log("Hello, " + firstName);
+// function greet(firstName: string){
+//     console.log("Hello, " + firstName);
 
-}
+// }
 
-greet("Anubav")
+// greet("Anubav")
 
-function sum(a: number, b:number): number{
-    return a + b
-}
+// function sum(a: number, b:number): number{
+//     return a + b
+// }
 
-console.log(sum(3, 6));
+// console.log(sum(3, 6));
 
-function isLegal(age: number) {
-    if (age > 18) {
-        return true;
-    } else {
-        return false
-    }
-}
+// function isLegal(age: number) {
+//     if (age > 18) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
 
-console.log(isLegal(2));
+// console.log(isLegal(2));
 
-function runAfter1s(fn: ()=>void){
-    setTimeout(fn, 1000)
-}
+// function runAfter1s(fn: ()=>void){
+//     setTimeout(fn, 1000)
+// }
 
-runAfter1s(function(){
-    console.log("hello");
+// runAfter1s(function(){
+//     console.log("hello");
 
-})
+// })
 
-interface
+// interface
 
-interface User {
-  firstName: string;
-  lastName: string;
-  age: number;
-  email?: string;
-}
+// interface User {
+//   firstName: string;
+//   lastName: string;
+//   age: number;
+//   email?: string;
+// }
 
-function isLegal(user: User) {
-  if (user.age > 18) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function isLegal(user: User) {
+//   if (user.age > 18) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-function greet(user: User) {
-  console.log("Hello," + user.firstName);
-}
+// function greet(user: User) {
+//   console.log("Hello," + user.firstName);
+// }
 
-isLegal({
-  firstName: "Anubhav",
-  lastName: "Singh",
-  age: 20,
-});
+// isLegal({
+//   firstName: "Anubhav",
+//   lastName: "Singh",
+//   age: 20,
+// });
 
-type StringOrNumber = string | number;
+// type StringOrNumber = string | number;
 
-function printId(id: StringOrNumber) {
-  console.log(`ID: ${id}`);
-}
+// function printId(id: StringOrNumber) {
+//   console.log(`ID: ${id}`);
+// }
 
-printId(101); // ID: 101
-printId("202"); // ID: 202
+// printId(101); // ID: 101
+// printId("202"); // ID: 202
 
-// you can't doing using this
-type Employee = {
-  name: string;
-  startDate: Date;
-};
+// // you can't doing using this
+// type Employee = {
+//   name: string;
+//   startDate: Date;
+// };
 
-type Manager = {
-  name: string;
-  department: string;
-};
+// type Manager = {
+//   name: string;
+//   department: string;
+// };
 
-type TeamLead = Employee & Manager;
+// type TeamLead = Employee & Manager;
 
-const teamLead: TeamLead = {
-  name: "harkirat",
-  startDate: new Date(),
-  department: "Software developer",
-};
+// const teamLead: TeamLead = {
+//   name: "harkirat",
+//   startDate: new Date(),
+//   department: "Software developer",
+// };
 
 
 
-// interface ❌
-// type☑️
+// // interface ❌
+// // type☑️
 
-function maxValue(arr: number[]) {
-  let max = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
+// function maxValue(arr: number[]) {
+//   let max = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
 
-console.log(maxValue([1, 2, 3]));
+// console.log(maxValue([1, 2, 3]));
 
 
 // important :
 // what is difference between type and interface in typescript
+
+
+// // enum
+// enum Direction {
+//     Up,
+//     Down,
+//     Left,
+//     Right
+// }
+
+// function doSomething(keyPressed: Direction) {
+// 	// do something.
+
+//     if(keyPressed == Direction.Up)
+// }
+// doSomething(Direction.Up)
+
+
+// *******************************
+// type Input = number | string;
+
+// function firstEl(arr: Input){
+//     return arr[0];
+// }
+
+// const value = firstEl(["Anubhav"])
+// value.toUpperCase() // not givin the answer
+
+// to solving this problem 
+
+// function identity<T>(arg: T): T {
+//     return arg;
+// }
+
+// let output1 = identity<string>("myString");
+// let output2 = identity<number>(100);
+
+// console.log(output1.toUpperCase)
+
+
+// solving this question
+
+
+function firstEl<T>(arr: T[]){
+    return arr[0];
+}
+
+const value = firstEl<string>(["Anubhav", "Singh"])
+const value2 = firstEl(["Anubhav","kumar", "Singh"])
+const value3 = firstEl([1, 2, 3])
+
+console.log(value.toUpperCase());
+
